21cfcdad8aba9849e839ba5b86c83b093594d6c4
Ritesh Ranjan <ranjanritesh43@gmail.com>
2024-10-02 14:15:01 +0530

    First class function

--- functions-JS/function-JS.js
+++ functions-JS/function-JS.js
@@ -12,5 +12,46 @@ function functionExampleTwo() {
     var numberFuntionExample = 10;
     console.log(numberFuntionExample);
 }
-
 // Execution Context Play Imp Role In this.
+
+/* ***************************************** First Class Function ft Anonymous Functions ***********************/
+
+//Function Statement aka Function Declaration:
+function functionalStatement() {
+    console.log("this is what functional statement or function declaration is")
+}
+functionalStatement();
+
+//function Expression:
+var functionExpresion = function () {
+    console.log("this is what function expression without name that is anonymous function attached to it as value") // Anonmyous function can be store as variable or function as value.
+}
+functionExpresion();
+
+//Anonymous Function:
+/*
+function () {
+}
+*/
+
+//Named Function Expression:
+var namedfunctionExpresion = function namedfunction() {
+    console.log("this is what function expression with name is") // Anonmyous function can be store as variable or function as value.
+}
+namedfunctionExpresion();
+// namedfunction() -> it will throw reference error as it decalred as variable or function with value it is not in the global scope
+
+//Parameters and arguments:
+function takingInputFunction(param1,param2) { // accepting these parameters 
+    return param1 * param2;
+}
+console.log(takingInputFunction(3,3)) // giving argument
+
+//First class function:
+let firstClassFunctionExp = function outer(param) {
+                                return function inner() { }
+                            }
+console.log(firstClassFunctionExp());
+//functions are First Class Citizens
+
+//Arrow functions:
--- functions-JS/readME.txt
+++ functions-JS/readME.txt
@@ -1,2 +1,11 @@
 1) Function Invokation creates a Execution Context which push in the call stack once it is done the call stack get empty it
 2) Indepent Execution Context with its own local spaces and memory 
+
+3)First Class Function ft. Anonymous Function.
+ //Functional Statement
+ //Function Expression
+ //Anonymous Function
+ //Named Function Expression
+ //Parameters and arguments
+ //First class function
+ //Arrow Functions
